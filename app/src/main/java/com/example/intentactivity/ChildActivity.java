f7c508873a6f1421a4856b503dd6c3da3cb38f7f
HanNhat44 <hannhat2468@gmail.com>
2022-09-16 08:39:44 +0700

    commit 1

---
+++ app/src/main/java/com/example/intentactivity/ChildActivity.java
@@ -0,0 +1,28 @@
+package com.example.intentactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChildActivity extends AppCompatActivity {
+    Button btnChild;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        btnChild = (Button) findViewById(R.id.buttonChild);
+
+        btnChild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i  = new Intent(ChildActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
